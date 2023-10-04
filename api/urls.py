34a13fec6b421bbf01b8d59c9dbb1bb73b13a940
mateusmouraplug <dev30@plugthink.com.br>
2023-10-04 15:39:38 -0300

    feat: Added patients and users urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
-from api.ger_therapists.urls import urlpatterns as therapists_url 
+
+
+from api.ger_therapists.urls import urlpatterns as therapists_urls
+from api.ger_patients.urls import urlpatterns as patients_urls
+from api.users.urls import urlpatterns as users_urls
 
 api_urlpatterns = []
 
-api_urlpatterns += therapists_url
+api_urlpatterns += therapists_urls
+api_urlpatterns += patients_urls
+api_urlpatterns += users_urls
 
 urlpatterns = [path("api/", include(api_urlpatterns))]
