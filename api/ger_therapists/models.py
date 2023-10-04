70a44900c23cf398ae393a2a8348eaddd4cf273a
mateusmouraplug <dev30@plugthink.com.br>
2023-10-04 16:25:13 -0300

    fix: Changed from OneToOne to ForeignKey

--- api/ger_therapists/models.py
+++ api/ger_therapists/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from api.users.models import User
+from api.models import User
 
 class Therapist(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     specialization = models.CharField(max_length=100, null=True)
     # Add more fields as needed
