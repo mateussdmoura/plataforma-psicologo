0cbf04ee98d489606c0bf7ceceb7894f7a344895
mateusmouraplug <dev30@plugthink.com.br>
2023-10-04 15:38:53 -0300

    feat: User Foreign Key

--- api/ger_therapists/models.py
+++ api/ger_therapists/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from api.users.models import User
 
 class Therapist(models.Model):
-    name = models.CharField(max_length=100)
-    specialization = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=100, null=True)
     # Add more fields as needed
