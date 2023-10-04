64fd029dfc055c92363c874be57b9c33969bd590
mateusmouraplug <dev30@plugthink.com.br>
2023-10-04 20:47:55 -0300

    feat: Implemented initial appointment model

---
+++ api/appointments/models.py
@@ -0,0 +1,22 @@
+# models.py
+from django.db import models
+from api.ger_therapists.models import Therapist
+from api.ger_patients.models import Patient
+from enum import Enum
+
+class AppointmentState(Enum):
+    SCHEDULED = 'Scheduled'
+    ONGOING = 'Ongoing'
+    CANCELLED = 'Cancelled'
+    DONE = 'Done'
+
+class Appointment(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    therapist = models.ForeignKey(Therapist, on_delete=models.CASCADE)
+    state = models.CharField(
+        max_length=20,
+        choices=[(state.value, state.name) for state in AppointmentState],
+        default=AppointmentState.SCHEDULED.value
+    )
