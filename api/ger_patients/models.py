df4263ca068c180b34623bcd02f17c4d3ae9587c
mateusmouraplug <dev30@plugthink.com.br>
2023-10-04 16:23:48 -0300

    feat: Changed User's model location

--- api/ger_patients/models.py
+++ api/ger_patients/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from api.users.models import User
+from api.models import User
 
 class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # Add more fields as needed
