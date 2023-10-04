46e2e0db7c8916f2702aa52060c0badba12554fb
mateusmouraplug <dev30@plugthink.com.br>
2023-10-04 16:24:18 -0300

    feat: Created viewset

--- api/ger_patients/views.py
+++ api/ger_patients/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
-from .models import Therapist
+from .models import Patient
 from .serializers import PatientSerializer
 
 class PatientViewSet(viewsets.ModelViewSet):
-    queryset = Therapist.objects.all()
+    queryset = Patient.objects.all()
     serializer_class = PatientSerializer
