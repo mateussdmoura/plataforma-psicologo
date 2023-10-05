4b7649b4c76570a0624cff922af9827fd8539bf0
mateusmouraplug <dev30@plugthink.com.br>
2023-10-05 15:01:22 -0300

    feat: Implemented appointments urls

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,13 @@ from django.urls import path, include
 from api.ger_therapists.urls import urlpatterns as therapists_urls
 from api.ger_patients.urls import urlpatterns as patients_urls
 from api.users.urls import urlpatterns as users_urls
+from api.appointments.urls import urlpatterns as appointments_urls
 
 api_urlpatterns = []
 
 api_urlpatterns += therapists_urls
 api_urlpatterns += patients_urls
 api_urlpatterns += users_urls
+api_urlpatterns += appointments_urls
 
 urlpatterns = [path("api/", include(api_urlpatterns))]
