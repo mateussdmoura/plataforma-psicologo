a751d3745fbaee46d954679186030e28846d1d45
mateusmouraplug <dev30@plugthink.com.br>
2023-10-05 15:01:40 -0300

    feat: Implemented basic CRUD

--- api/appointments/views.py
+++ api/appointments/views.py
@@ -1,7 +1,50 @@
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import status
 from appointments.models import Appointment
 from .serializers import AppointmentSerializer
 
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Optionally, restrict access to authenticated users
+
+    # Retrieve a single appointment by its ID
+    def retrieve(self, request, pk=None):
+        try:
+            appointment = Appointment.objects.get(pk=pk)
+        except Appointment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(appointment)
+        return Response(serializer.data)
+
+    # Create a new appointment
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # Update an existing appointment by its ID
+    def update(self, request, pk=None):
+        try:
+            appointment = Appointment.objects.get(pk=pk)
+        except Appointment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(appointment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    # Delete an appointment by its ID
+    def destroy(self, request, pk=None):
+        try:
+            appointment = Appointment.objects.get(pk=pk)
+        except Appointment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        appointment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
