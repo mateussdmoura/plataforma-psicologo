bcab3c07bfd40b1c05902a6ba64ce09084e436cf
mateusmouraplug <dev30@plugthink.com.br>
2023-10-05 14:35:43 -0300

    feat: Modified Enum values and added REQUESTED; Added requested_by and save method to correctly save the user that requested the appointment

--- appointments/models.py
+++ appointments/models.py
@@ -1,21 +1,35 @@
 from django.db import models
 from api.ger_therapists.models import Therapist
 from api.ger_patients.models import Patient
+from api.models import User
 from enum import Enum
 
 class AppointmentState(Enum):
-    SCHEDULED = 'Scheduled'
-    ONGOING = 'Ongoing'
-    CANCELLED = 'Cancelled'
-    DONE = 'Done'
+    REQUESTED = "REQUESTED"
+    SCHEDULED = "SCHEDULED"
+    ONGOING = "ONGOING"
+    CANCELLED = "CANCELLED"
+    DONE = "DONE"
 
 class Appointment(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     therapist = models.ForeignKey(Therapist, on_delete=models.CASCADE)
+    requested_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     state = models.CharField(
         max_length=20,
         choices=[(state.value, state.name) for state in AppointmentState],
         default=AppointmentState.SCHEDULED.value
     )
+    
+    def save(self, *args, **kwargs):
+        # Check if the requested_by field is not set
+        if not self.requested_by:
+            # You can access the user who sent the request using the request object
+            user = kwargs.get('user')  # Assuming 'user' is passed in as a keyword argument
+            if user:
+                self.requested_by = user
+
+    def __str__(self):
+        return f"[{self.state}] Appointment for {self.patient} with {self.therapist}"
